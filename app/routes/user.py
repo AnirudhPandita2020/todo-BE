74078bdcae03b6eeb70e6b24bae2740858e35043
Anirudh <kppkanu@gmail.com>
2022-03-08 11:03:06 +0530

    Removed Same username Function

--- app/routes/user.py
+++ app/routes/user.py
@@ -10,13 +10,12 @@ router = APIRouter(prefix="/users",tags = ["USER"])
 @router.post("/",status_code=status.HTTP_201_CREATED,response_model=schemas.User)
 async def create_user(user:schemas.UserCreate,db:Session =Depends(database.get_db)):
     exists_user = db.query(models.User).filter(models.User.email == user.email).first()
-    same_username = db.query(models.User).filter(models.User.username == user.username).first()
+    
     
     if exists_user:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail="User already exists")
     
-    if same_username:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail="Username already taken")
+    
     
     user.password = utils.hash(user.password)
     new_user = models.User(**user.dict())
@@ -31,3 +30,4 @@ async def get_user(id:int,db:Session = Depends(database.get_db)):
     if not user:
         raise HTTPException(status.HTTP_404_NOT_FOUND,"User not found")
     return user
+
